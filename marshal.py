844ae1a4eea39828532e317fc3e84c832efc8da0
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2019-07-09 14:47:30 +0700

    Update marshal.py

--- marshal.py
+++ marshal.py
@@ -1,28 +1,32 @@
 import os
-import sys
-import time
-import json
-import urllib
 
-def pesan():
+try:
+    import requests
+    import json
+except:
+       os.system('pip2 install requests')
+       print 'please run again'
 
-    try:
-        while True:
-            pes = raw_input(" [@] Saya: ")
-            url = urllib.urlopen("http://sandbox.api.simsimi.com/request.p?key=KUNCI_API&lc=id&ft=1.0&text=" + pes)
-            jsl = json.loads(url.read())
-            print " [$] Bebeb: " + jsl["response"]
-    
-    except KeyboardInterrupt:
-        print
-        print " Keluar Dari Bot.."
-        sys.exit()
 
-def main():
-    pesan()
+class main(object):
 
-if __name__=="__main__":
-    main()
+      def __init__(self):
+
+          def chatkuy(user,bots):
+              self.key = "32556fbb-a9ad-41ea-9def-ccf78e0f8f4e"
+              while True:
+                     self.msg = raw_input('[\033[93m%s\033[97m] : '%(user))
+                     self.url = 'http://sandbox.api.simsimi.com/request.p?key=%s&lc=id&ft=1.0&text=%s'%(self.key,self.msg)
+                     self.reqs = json.loads(requests.get(self.url).text)
+                     print '[\033[92m%s\033[97m] : %s'%(bots,self.reqs['response'])
 
 
 
+          user = raw_input('enter your name : ')
+          bots = raw_input('enter bot name : ')
+          print ''
+          chatkuy(user,bots)
+
+if __name__=='__main__':
+   os.system('clear')
+   main()
