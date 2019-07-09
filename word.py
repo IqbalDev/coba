ca60dcf3e07bdf641f24a70f9c51171a17bd1aa0
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2019-07-09 23:29:47 +0700

    Update word.py

--- word.py
+++ word.py
@@ -1,6 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Author Iqbal Dev
+# Tools Make Wordlist
+
 import os
 import sys
 import time
+
+d = "\033[90m", m = "\033[91m", h = "\033[32m", k = "\033[33m", p = "\033[97m"
 
 data = []
 count = 0
@@ -24,8 +30,8 @@ def perulangan():
                 print 
                 dat = open("list.txt", "r")
                 da = dat.readlines()
-                print " ================================="
-                print " [#] Jumlah Data: ", len(da)
+                print h+" ================================="
+                print p+" ["+k+"#"+p+"]"+h+" Jumlah Data: ", len(da)
                 word = raw_input(" ["+str(count)+"] Masukkan Data: ")
                 count += 1
                 if word == "1":
@@ -33,38 +39,40 @@ def perulangan():
                     sys.exit()
 
                 else: 
-                    print " Fail..."
                     print
                     data.append(word)
                     tam = open("list.txt", "a")
                     tam.write(word + "\n")
                     tam.close()
                     for i in data:
-                        print " [#] [ " + i + " ]"
+                        print p+" ["+k+"#"+p+"]"+h " [ " + i + " ]"
 
         except KeyboardInterrupt:
             try:
                 def opsi():
                             
                     print
-                    print " ============================="
-                    print "  [1] Hapus Data...?"
+                    print h+" ============================="
+                    print p+"  [1] Hapus Data...?"
                     print "  [2] Keluar Dari Program.."
                     print "  [3] Kembali Ke Program"
-                    print " ============================="
+                    print h+" ============================="
                     op = raw_input(" Pilih => ")
                     if op == "1":
-                        os.system("del list.txt")
                         os.system("rm -f list.txt")
                         print " Data Terhapus"
+                        
                     elif op == "2":
                         sys.exit()
                         print " Keluar Dari Program"
+                        
                     elif op == "3":
                         perulangan()
+                        
                     else:
                         print " Salah Ndolll"
                         opsi()
+                        
                 opsi()
 
             except KeyboardInterrupt:
